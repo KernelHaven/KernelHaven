2f8435f0ee85a4443cb7ffe54ddc575b78329dcb
Adam <adam@sse.uni-hildesheim.de>
2017-10-05 10:43:56 +0200

    Log extractor output and exceptions in EmptyAnalysis

--- src/net/ssehub/kernel_haven/analysis/EmptyAnalysis.java
+++ src/net/ssehub/kernel_haven/analysis/EmptyAnalysis.java
@@ -1,11 +1,14 @@
 package net.ssehub.kernel_haven.analysis;
 
 import net.ssehub.kernel_haven.SetUpException;
+import net.ssehub.kernel_haven.build_model.BuildModel;
 import net.ssehub.kernel_haven.code_model.SourceFile;
 import net.ssehub.kernel_haven.config.Configuration;
+import net.ssehub.kernel_haven.util.ExtractorException;
+import net.ssehub.kernel_haven.variability_model.VariabilityModel;
 
 /**
- * An empty analysis which does nothing but start all extrators.
+ * An empty analysis which does nothing but start all extractors. Logs extractor output and exceptions.
  *
  * @author Adam
  */
@@ -27,13 +30,50 @@ public class EmptyAnalysis extends AbstractAnalysis {
             bmProvider.start();
             vmProvider.start();
             
-            // wait for result
-            vmProvider.getResult();
-            bmProvider.getResult();
+            // read results
+            VariabilityModel vm = vmProvider.getResult();
+            BuildModel bm = bmProvider.getResult();
+            
+            int numCm = 0;
             SourceFile result;
             do {
                 result = cmProvider.getNextResult();
+                if (result != null) {
+                    numCm++;
+                }
             } while (result != null);
+            
+            if (vm != null) {
+                LOGGER.logInfo("Got a variability model with " + vm.getVariables().size() + " variables");
+            } else {
+                LOGGER.logInfo("Got no variability model");
+            }
+            
+            if (bm != null) {
+                LOGGER.logInfo("Got a build model with " + bm.getSize() + " files");
+            } else {
+                LOGGER.logInfo("Got no build model");
+            }
+            
+            LOGGER.logInfo("Got " + numCm + " source files in the code model");
+            
+            ExtractorException vmExc = vmProvider.getException();
+            if (vmExc != null) {
+                LOGGER.logExceptionInfo("Got an exception from the variability model extractor", vmExc);
+            }
+            
+            ExtractorException bmExc = bmProvider.getException();
+            if (bmExc != null) {
+                LOGGER.logExceptionInfo("Got an exception from the build model extractor", bmExc);
+            }
+            
+            ExtractorException cmExc;
+            do {
+                cmExc = cmProvider.getNextException();
+                if (cmExc != null) {
+                    LOGGER.logExceptionInfo("Got an exception from the code model extractor", cmExc);
+                }
+            } while (cmExc != null);
             
         } catch (SetUpException e) {
             LOGGER.logException("Exception while starting extractors", e);
