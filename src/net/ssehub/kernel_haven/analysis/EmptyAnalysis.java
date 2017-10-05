02312fd2d39d5723fc51d4b32ecfcf0bbc6bd6c9
Adam <adam@sse.uni-hildesheim.de>
2017-10-05 10:44:48 +0200

    Directly log output in EmptyAnalysis

--- src/net/ssehub/kernel_haven/analysis/EmptyAnalysis.java
+++ src/net/ssehub/kernel_haven/analysis/EmptyAnalysis.java
@@ -31,8 +31,20 @@ public class EmptyAnalysis extends AbstractAnalysis {
             vmProvider.start();
             
             // read results
+            
             VariabilityModel vm = vmProvider.getResult();
+            if (vm != null) {
+                LOGGER.logInfo("Got a variability model with " + vm.getVariables().size() + " variables");
+            } else {
+                LOGGER.logInfo("Got no variability model");
+            }
+            
             BuildModel bm = bmProvider.getResult();
+            if (bm != null) {
+                LOGGER.logInfo("Got a build model with " + bm.getSize() + " files");
+            } else {
+                LOGGER.logInfo("Got no build model");
+            }
             
             int numCm = 0;
             SourceFile result;
@@ -42,20 +54,9 @@ public class EmptyAnalysis extends AbstractAnalysis {
                     numCm++;
                 }
             } while (result != null);
-            
-            if (vm != null) {
-                LOGGER.logInfo("Got a variability model with " + vm.getVariables().size() + " variables");
-            } else {
-                LOGGER.logInfo("Got no variability model");
-            }
-            
-            if (bm != null) {
-                LOGGER.logInfo("Got a build model with " + bm.getSize() + " files");
-            } else {
-                LOGGER.logInfo("Got no build model");
-            }
-            
             LOGGER.logInfo("Got " + numCm + " source files in the code model");
+            
+            // read exceptions
             
             ExtractorException vmExc = vmProvider.getException();
             if (vmExc != null) {
