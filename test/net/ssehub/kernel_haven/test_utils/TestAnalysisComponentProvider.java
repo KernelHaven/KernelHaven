c11fecf85b3af2e9a2bc14cefe6c3b12c6116dd5
Adam <adam@sse.uni-hildesheim.de>
2017-10-05 15:08:23 +0200

    Add TestAnalysisComponentProvider to make it easier to write test cases

---
+++ test/net/ssehub/kernel_haven/test_utils/TestAnalysisComponentProvider.java
@@ -0,0 +1,57 @@
+package net.ssehub.kernel_haven.test_utils;
+
+import java.util.Properties;
+
+import net.ssehub.kernel_haven.SetUpException;
+import net.ssehub.kernel_haven.TestConfiguration;
+import net.ssehub.kernel_haven.analysis.AnalysisComponent;
+import net.ssehub.kernel_haven.util.BlockingQueue;
+
+/**
+ * An analysis component that provides static data. Useful as input for testing other components.
+ * 
+ * @param <T> The type of data to create.
+ * 
+ * @author Adam
+ */
+public class TestAnalysisComponentProvider<T> extends AnalysisComponent<T> {
+
+    private BlockingQueue<T> data;
+    
+    /**
+     * Creates a new instance with the given data.
+     * 
+     * @param data The data that this component should "create" and pass to the next component.
+     * 
+     * @throws SetUpException Shouldn't happen.
+     */
+    public TestAnalysisComponentProvider(Iterable<T> data) throws SetUpException {
+        super(new TestConfiguration(new Properties()));
+        this.data = new BlockingQueue<>();
+        for (T t : data) {
+            this.data.add(t);
+        }
+        this.data.end();
+    }
+        /**
+     * Creates a new instance with the given data.
+     * 
+     * @param data The data that this component should "create" and pass to the next component.
+     * 
+     * @throws SetUpException Shouldn't happen.
+     */
+    public TestAnalysisComponentProvider(T data) throws SetUpException {
+        super(new TestConfiguration(new Properties()));
+        this.data = new BlockingQueue<>();
+        this.data.add(data);
+        this.data.end();
+    }
+
+    @Override
+    protected void execute() {
+        for (T element : this.data) {
+            addResult(element);
+        }
+    }
+    
+}
