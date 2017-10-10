a05efe0fe00fd2ad43f984d6a83d0f8488a6dbd7
Elscha <elscha@sse.uni-hildesheim.de>
2017-10-10 10:09:59 +0200

    Test case for the configuration

--- test/net/ssehub/kernel_haven/config/ConfigurationTest.java
+++ test/net/ssehub/kernel_haven/config/ConfigurationTest.java
@@ -24,15 +24,22 @@ public class ConfigurationTest {
     public void testBooleanValues() throws SetUpException {
         Setting<Boolean> falseValue = new Setting<>("value.false", BOOLEAN, false, "false", "");
         Setting<Boolean> trueValue = new Setting<>("value.true", BOOLEAN, false, "true", "");
+        Setting<Boolean> overwriteableValue = new Setting<>("value.x", BOOLEAN, false, "true", "");
         
-        Configuration config = new Configuration(new Properties());
+        Properties prop = new Properties();
+        prop.setProperty(overwriteableValue.getKey(), "false");
+        Configuration config = new Configuration(prop);
         config.registerSetting(falseValue);
         config.registerSetting(trueValue);
+        config.registerSetting(overwriteableValue);
         
         boolean value = config.getValue(falseValue);
         Assert.assertFalse("False value is treated as true", value);
         
         value = config.getValue(trueValue);
         Assert.assertTrue("True value is treated as false", value);
+        
+        value = config.getValue(overwriteableValue);
+        Assert.assertFalse("False value is treated as true", value);
     }
 }
