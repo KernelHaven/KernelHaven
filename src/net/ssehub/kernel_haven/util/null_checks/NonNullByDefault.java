5221c132bcbd4f80aa21e3e1b254c90519cbed3f
adam-sse <adam@sse.uni-hildesheim.de>
2018-01-15 14:59:59 +0100

    Allow @NonNullByDefault on types and methods, too

--- src/net/ssehub/kernel_haven/util/null_checks/NonNullByDefault.java
+++ src/net/ssehub/kernel_haven/util/null_checks/NonNullByDefault.java
@@ -1,6 +1,8 @@
 package net.ssehub.kernel_haven.util.null_checks;
 
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import java.lang.annotation.Retention;
@@ -12,7 +14,7 @@ import java.lang.annotation.Target;
  * @author Adam
  */
 @Retention(SOURCE)
-@Target({ PACKAGE })
+@Target({ PACKAGE, TYPE, METHOD })
 public @interface NonNullByDefault {
 
 }
