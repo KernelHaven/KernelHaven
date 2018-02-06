3a78f712709f95838729a5ee092fdea40820231a
adam-sse <adam@sse.uni-hildesheim.de>
2018-02-06 12:58:29 +0100

    Add name attribute to function

--- src/net/ssehub/kernel_haven/code_model/ast/Function.java
+++ src/net/ssehub/kernel_haven/code_model/ast/Function.java
@@ -5,20 +5,27 @@ import net.ssehub.kernel_haven.util.null_checks.NonNull;
 
 public class Function extends SyntaxElementWithChildreen {
 
+    private @NonNull String name;
+    
     private @NonNull SyntaxElement header;
     
-    public Function(@NonNull Formula presenceCondition, @NonNull SyntaxElement header) {
+    public Function(@NonNull Formula presenceCondition, @NonNull String name, @NonNull SyntaxElement header) {
         super(presenceCondition);
         this.header = header;
+        this.name = name;
     }
     
     public @NonNull SyntaxElement getHeader() {
         return header;
     }
+    
+    public @NonNull String getName() {
+        return name;
+    }
 
     @Override
     protected @NonNull String elementToString(@NonNull String indentation) {
-        return "Function\n" + header.toString(indentation + "\t");
+        return "Function " + name + "\n" + header.toString(indentation + "\t");
     }
 
     @Override
