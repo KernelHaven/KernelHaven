e17379aaf0b7a73103665832e712e67847e7ca31
Adam <adam@sse.uni-hildesheim.de>
2018-02-09 21:25:57 +0100

    Add missing Javadoc comments to ISyntaxElementVisitor

--- src/net/ssehub/kernel_haven/code_model/ast/ISyntaxElementVisitor.java
+++ src/net/ssehub/kernel_haven/code_model/ast/ISyntaxElementVisitor.java
@@ -66,6 +66,13 @@ public interface ISyntaxElementVisitor {
         }
     }
     
+    /**
+     * <b>C-Code:</b> Visits a function. These are usually at the top level of a file.
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param function The function to visit.
+     */
     public default void visitFunction(@NonNull Function function) {
         function.getHeader().accept(this);
         for (int i = 0; i < function.getNestedElementCount(); i++) {
@@ -73,16 +80,38 @@ public interface ISyntaxElementVisitor {
         }
     }
     
+    /**
+     * <b>C-Code:</b> Visits a single statement. This represents assignments, declarations, etc. (basically everything
+     * that ends with a semicolon and does something).
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param statement The statement to visit.
+     */
     public default void visitSingleStatement(@NonNull SingleStatement statement) {
         statement.getCode().accept(this);
     }
     
+    /**
+     * <b>C-Code:</b> Visits a compound statement. This is a block of statements
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param block The block to visit.
+     */
     public default void visitCompoundStatement(@NonNull CompoundStatement block) {
         for (int i = 0; i < block.getNestedElementCount(); i++) {
             block.getNestedElement(i).accept(this);
         }
     }
     
+    /**
+     * <b>C-Code:</b> Visits a list of unparsed code objects.
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param code The code to visit.
+     */
     public default void visitCodeList(@NonNull CodeList code) {
         for (int i = 0; i < code.getNestedElementCount(); i++) {
             code.getNestedElement(i).accept(this);
@@ -96,6 +125,13 @@ public interface ISyntaxElementVisitor {
      */
     public void visitCode(@NonNull Code code);
     
+    /**
+     * <b>C-Code:</b> Visits a type definition.
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param typeDef The type definiton to visit.
+     */
     public default void visitTypeDefinition(@NonNull TypeDefinition typeDef) {
         typeDef.getDeclaration().accept(this);
         for (int i = 0; i < typeDef.getNestedElementCount(); i++) {
@@ -103,12 +139,37 @@ public interface ISyntaxElementVisitor {
         }
     }
 
+    /**
+     * <b>C-Code:</b> Visits a label.
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param label The label to visit.
+     */
     public default void visitLabel(@NonNull Label label) {
         label.getCode().accept(this);
     }
     
+    /**
+     * <b>C-Code:</b> Visits a comment.
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param comment The Comment to visit.
+     */
+    public default void visitComment(@NonNull Comment comment) {
+        comment.getComment().accept(this);
+    }
+    
     // C Control structures
     
+    /**
+     * <b>C control structure:</b> Visits a branching statement (if, else if or else).
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param branchStatement The branching statement to visit.
+     */
     public default void visitBranchStatement(@NonNull BranchStatement branchStatement) {
         ICode condition = branchStatement.getIfCondition();
         if (null != condition) {
@@ -120,6 +181,13 @@ public interface ISyntaxElementVisitor {
         }
     }
     
+    /**
+     * <b>C control structure:</b> Visits a switch statement.
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param switchStatement The switch to visit.
+     */
     public default void visitSwitchStatement(@NonNull SwitchStatement switchStatement) {
         switchStatement.getHeader().accept(this);
         
@@ -128,6 +196,13 @@ public interface ISyntaxElementVisitor {
         }
     }
     
+    /**
+     * <b>C control structure:</b> Visits a case statement.
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param caseStatement The case statement to visit.
+     */
     public default void visitCaseStatement(@NonNull CaseStatement caseStatement) {
         ICode condition = caseStatement.getCaseCondition();
         if (null != condition) {
@@ -139,6 +214,13 @@ public interface ISyntaxElementVisitor {
         }
     }
     
+    /**
+     * <b>C control structure:</b> Visits a loop statement (while, do-while or for).
+     * <br/>
+     * Provides default Visitation: <b>true</b>
+     * 
+     * @param loop The loop to visit.
+     */
     public default void visitLoopStatement(@NonNull LoopStatement loop) {
         loop.getLoopCondition().accept(this);
         
@@ -147,7 +229,4 @@ public interface ISyntaxElementVisitor {
         }
     }
     
-    public default void visitComment(@NonNull Comment comment) {
-        comment.getComment().accept(this);
-    }
 }
