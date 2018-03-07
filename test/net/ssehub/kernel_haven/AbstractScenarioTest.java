4a0bed578c01009daa9c5f0f24c219782e7e7a1a
Elscha <elscha@sse.uni-hildesheim.de>
2018-03-07 13:28:16 +0100

    Trying to get rid of Jenkins bug

--- test/net/ssehub/kernel_haven/AbstractScenarioTest.java
+++ test/net/ssehub/kernel_haven/AbstractScenarioTest.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 
 import net.ssehub.kernel_haven.util.Util;
 
@@ -96,7 +95,6 @@ public abstract class AbstractScenarioTest {
      * 
      * @throws Exception unwanted.
      */
-    @Test
     public void run()
         // CHECKSTYLE:OFF // we allow throws exception here
             throws Exception {
