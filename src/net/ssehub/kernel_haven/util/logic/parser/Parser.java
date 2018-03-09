b1843c21d09ce8313a5badcc40ade139cc4378cb
adam-sse <adam@sse.uni-hildesheim.de>
2018-03-09 13:32:48 +0100

    Better exception messages in Parser

--- src/net/ssehub/kernel_haven/util/logic/parser/Parser.java
+++ src/net/ssehub/kernel_haven/util/logic/parser/Parser.java
@@ -1,5 +1,7 @@
 package net.ssehub.kernel_haven.util.logic.parser;
 
+import static net.ssehub.kernel_haven.util.null_checks.NullHelpers.notNull;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -37,7 +39,7 @@ public class Parser<T> {
      */
     public T parse(@NonNull String expression) throws ExpressionFormatException  {
         Token[] tokens = lex(expression);
-        T f = parse(tokens, 0, tokens.length - 1);
+        T f = parse(tokens, 0, tokens.length - 1, expression);
         return f;
     }
 
@@ -89,8 +91,7 @@ public class Parser<T> {
                 i++;
                 
             } else {
-                throw new ExpressionFormatException("Invalid character in expression: " + expr[i]);
-                
+                throw makeException(expression, "Invalid character in expression: '" + expr[i] + "'", i);
             }
         }
         
@@ -118,26 +119,33 @@ public class Parser<T> {
      * @param tokens The flat array of tokens; the output of {@link #lex(String)}.
      * @param min The lower bound of the part of <code>tokens</code> that should be parsed, inclusive.
      * @param max The upper bound of the part of <code>tokens</code> that should be parsed, inclusive.
+     * @param expression The expression that is currently parsed. Used for error messages.
      * @return The parsed expression.
      * 
      * @throws ExpressionFormatException If the expression denoted by tokens is malformed.
      */
     // CHECKSTYLE:OFF
-    private T parse(Token[] tokens, int min, int max) throws ExpressionFormatException {
+    private T parse(Token[] tokens, int min, int max, @NonNull String expression) throws ExpressionFormatException {
         // this method calls itself recursively
 
         // if we have  no tokens left then something went wrong
         if (max - min < 0) {
-            throw new ExpressionFormatException("Expected identifier");
+            throw makeException(expression, "Expected identifier", -1);
         }
         
         // if we only have one token left, then it must be an identifier
         if (max - min == 0) {
             if (!(tokens[min] instanceof Identifier)) {
-                throw new ExpressionFormatException("Expected identifier, got " + tokens[min]);
+                throw makeException(expression, "Expected identifier, got " + tokens[min], -1);
             }
             
-            return grammar.makeIdentifierFormula(((Identifier) tokens[min]).getName());
+            try {
+                return grammar.makeIdentifierFormula(((Identifier) tokens[min]).getName());
+            } catch (ExpressionFormatException e) {
+                ExpressionFormatException newExc = makeException(expression, notNull(e.getMessage()), -1);
+                newExc.setStackTrace(e.getStackTrace());
+                throw newExc;
+            }
         }
         
         // find the "highest" operator in the bracket tree
@@ -156,7 +164,7 @@ public class Parser<T> {
                 }
                 
                 if (bracketDepth < 0) {
-                    throw new ExpressionFormatException("Unbalanced brackets");
+                    throw makeException(expression, "Unbalanced brackets", -1);
                 }
                 
             } else if (e instanceof Operator) {
@@ -181,12 +189,12 @@ public class Parser<T> {
         }
         
         if (bracketDepth != 0) {
-            throw new ExpressionFormatException("Unbalanced brackets");
+            throw makeException(expression, "Unbalanced brackets", -1);
         }
         
         T result = null;
 
-        result = createResult(tokens, min, max, ho);
+        result = createResult(tokens, min, max, ho, expression);
         
         return result;
     }
@@ -200,10 +208,14 @@ public class Parser<T> {
      * @param min The lower bound of the part of <code>tokens</code> that should be parsed, inclusive.
      * @param max The upper bound of the part of <code>tokens</code> that should be parsed, inclusive.
      * @param ho The highest operator.
+     * @param expression The expression that is currently parsed. Used for error messages.
+     * 
      * @return The result of the operator, as returned by the grammar.
      * @throws ExpressionFormatException If the grammar cannot parse this.
      */
-    private T createResult(Token[] tokens, int min, int max, HighestOperatorData ho) throws ExpressionFormatException {
+    private T createResult(Token[] tokens, int min, int max, HighestOperatorData ho, @NonNull String expression)
+            throws ExpressionFormatException {
+        
         T result;
         // if there is an operator that is not nested in any brackets
         if (ho.highestOperatorLevel == 0) {
@@ -211,36 +223,71 @@ public class Parser<T> {
             // and pass the results to the grammer to create the result
             
             if (ho.highestOp.isBinary()) {
-                T leftTree = parse(tokens, min, ho.highestOpPos - 1);
-                T rightTree = parse(tokens, ho.highestOpPos + 1, max);
-                result = grammar.makeBinaryFormula(ho.highestOp, leftTree, rightTree);
+                T leftTree = parse(tokens, min, ho.highestOpPos - 1, expression);
+                T rightTree = parse(tokens, ho.highestOpPos + 1, max, expression);
+                try {
+                    result = grammar.makeBinaryFormula(ho.highestOp, leftTree, rightTree);
+                } catch (ExpressionFormatException e) {
+                    ExpressionFormatException newExc = makeException(expression, notNull(e.getMessage()), -1);
+                    newExc.setStackTrace(e.getStackTrace());
+                    throw newExc;
+                }
                 
             } else {
                 if (ho.highestOpPos != min) {
-                    throw new ExpressionFormatException("Unary operator is not on the left");
+                    throw makeException(expression, "Unary operator is not on the left", -1);
                 }
                 
-                T childFormula = parse(tokens, min + 1, max);
-                result = grammar.makeUnaryFormula(ho.highestOp, childFormula);
+                T childFormula = parse(tokens, min + 1, max, expression);
+                try {
+                    result = grammar.makeUnaryFormula(ho.highestOp, childFormula);
+                } catch (ExpressionFormatException e) {
+                    ExpressionFormatException newExc = makeException(expression, notNull(e.getMessage()), -1);
+                    newExc.setStackTrace(e.getStackTrace());
+                    throw newExc;
+                }
                 
             }
         } else {
             // unpack the brackets and recursively call parse()
             
             if (!(tokens[min] instanceof Bracket) || !(tokens[max] instanceof Bracket)) {
-                throw new ExpressionFormatException("Couldn't find operator");
+                throw makeException(expression, "Couldn't find operator", -1);
             }
             
             Bracket first = (Bracket) tokens[min];
             Bracket last = (Bracket) tokens[max];
             
             if (first.isClosing() || !last.isClosing()) {
-                throw new ExpressionFormatException("Unbalanced brackets");
+                throw makeException(expression, "Unbalanced brackets", -1);
             }
             
-            result = parse(tokens, min + 1, max - 1);
+            result = parse(tokens, min + 1, max - 1, expression);
         }
         return result;
+    }
+    
+    /**
+     * Creates an {@link ExpressionFormatException}.
+     * 
+     * @param expression The formula that couldn't be parsed.
+     * @param message The message describing the exception.
+     * @param index The index where in the formula the exception occurred. -1 means unknown.
+     * 
+     * @return The exception with the proper error message.
+     */
+    private @NonNull ExpressionFormatException makeException(@NonNull String expression, @NonNull String message,
+            int index) {
+        
+        StringBuilder fullMessage = new StringBuilder(message).append("\nIn formula: ").append(expression);
+        if (index >= 0) {
+            fullMessage.append("\n            ");
+            for (int i = 0; i < index; i++) {
+                fullMessage.append(' ');
+            }
+            fullMessage.append('^');
+        }
+        return new ExpressionFormatException(fullMessage.toString());
     }
     
 }
