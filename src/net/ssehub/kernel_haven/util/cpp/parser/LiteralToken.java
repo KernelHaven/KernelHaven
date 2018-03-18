ff321bfb79a66ebf466f3ba15d895e771cdd2a78
Adam <adam@sse.uni-hildesheim.de>
2018-03-18 16:32:11 +0100

    Add missing equals() and hashCode() methods to LiteralToken

--- src/net/ssehub/kernel_haven/util/cpp/parser/LiteralToken.java
+++ src/net/ssehub/kernel_haven/util/cpp/parser/LiteralToken.java
@@ -1,6 +1,7 @@
 package net.ssehub.kernel_haven.util.cpp.parser;
 
 import net.ssehub.kernel_haven.util.null_checks.NonNull;
+import net.ssehub.kernel_haven.util.null_checks.Nullable;
 
 /**
  * A token representing a literal integer value.
@@ -38,6 +39,21 @@ final class LiteralToken extends CppToken {
     @Override
     public int getLength() {
         return length;
+    }
+    
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        boolean equal = super.equals(obj);
+        if (equal && obj instanceof LiteralToken) {
+            LiteralToken other = (LiteralToken) obj;
+            equal = other.value == this.value && this.length == other.length;
+        }
+        return equal;
+    }
+    
+    @Override
+    public int hashCode() {
+        return super.hashCode() + Long.hashCode(value) + Integer.hashCode(length);
     }
 
     @Override
