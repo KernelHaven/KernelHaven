aa8e777526649791341f22f7e247f3919add3682
adam-sse <adam@sse.uni-hildesheim.de>
2018-10-30 14:11:58 +0100

    Revert "Add some temporary debug output"
    This reverts commit e86695d21f43dafc5197af7a8efb475c9895348b.

--- src/net/ssehub/kernel_haven/util/OrderPreservingParallelizer.java
+++ src/net/ssehub/kernel_haven/util/OrderPreservingParallelizer.java
@@ -143,7 +143,6 @@ public class OrderPreservingParallelizer<Input, Output> {
     private void start(int numThreads) {
         // spawn worker threads
         for (int i = 0; i < numThreads; i++) {
-            Logger.get().logDebug("Spawning thread " + (i + 1)); // TODO
             new Thread(() -> {
                 
                 WorkPackage wp;
@@ -151,10 +150,9 @@ public class OrderPreservingParallelizer<Input, Output> {
                     wp.execute();
                     done.add(wp);
                 }
-            
+                
                 synchronized (OrderPreservingParallelizer.this) {
                     numWorkersDone++;
-                    Logger.get().logDebug("Worker " + numWorkersDone + " done"); // TODO
                     if (numWorkersDone == numThreads) {
                         done.end();
                     }
